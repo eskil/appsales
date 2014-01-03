ee1f568637763d941c80663415a6fa22b7efc1a5
Eskil Heyn Olsen <eskil@eskil.org>
2014-01-03 11:34:27 -0800

    Nokogiri eh

--- lib/appsales.rb
+++ lib/appsales.rb
@@ -30,33 +30,37 @@ class AppSales
 
   protected
 
-  def get_login_form
-    url = URI.parse('https://itunesconnect.apple.com')
+  # Try n times to load the url
+  def load_url(url, limit = 3)
+    raise Exception, 'Too many HTTP redirects' if limit == 0
 
-    (0..3).each do |limit|
-      raise Exception, 'Too many HTTP redirects' if limit == 3
+    url = (url.is_a? URI) ? url : URI.parse(url)
 
-      response = Net::HTTP.start(url.host, use_ssl: true) do |http|
-        http.get url.request_uri
-      end
-
-      case response
-      when Net::HTTPRedirection
-        new_url = URI.parse(response['location'])
-        if new_url.host
-          url = new_url
-        else
-          url.path = new_url.path
-        end
-      when Net::HTTPSuccess
-        doc = Nokogiri::HTML(response.body)
-        form = doc.xpath('//form[@name="appleConnectForm"]')
-        action = form[0]['action']
-        ...
-      else
-        # response code isn't a 200; raise an exception
-        response.error!
-      end
+    response = Net::HTTP.start(url.host, use_ssl: true) do |http|
+      http.get url.request_uri
     end
+
+    case response
+    when Net::HTTPRedirection
+      new_url = URI.parse(response['location'])
+      if new_url.host
+        url = new_url
+      else
+        url.path = new_url.path
+      end
+      load_url(url, limit - 1)
+    when Net::HTTPSuccess
+      return response.body
+    else
+      response.error!
+    end
+  end
+
+  def get_login_form(url = 'https://itunesconnect.apple.com')
+    page = load_url(url)
+    doc = Nokogiri::HTML(page)
+    form = doc.xpath('//form[@name="appleConnectForm"]')
+    action = form[0]['action']
+    puts action
   end
 end
