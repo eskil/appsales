ba23a6e086a80cf2af15b960baf8d89bd084e03d
Eskil Heyn Olsen <eskil@eskil.org>
2014-01-03 11:31:15 -0800

    Nokogiri eh

--- Gemfile
+++ Gemfile
@@ -1,5 +1,6 @@
-source "https://rubygems.org"
+source 'https://rubygems.org'
 
 gemspec
 
-gem "rspec"
+gem 'rspec'
+gem 'nokogiri'
--- lib/appsales.rb
+++ lib/appsales.rb
@@ -1,7 +1,12 @@
+require 'net/https'
+require 'nokogiri'
+
+
 class AppSales
   def initialize(username, password)
     @username = username
     @password = password
+    puts get_login_form
   end
 
   def vendor_id
@@ -21,5 +26,37 @@ class AppSales
   def report(type, date)
     puts type, date
     []
+  end
+
+  protected
+
+  def get_login_form
+    url = URI.parse('https://itunesconnect.apple.com')
+
+    (0..3).each do |limit|
+      raise Exception, 'Too many HTTP redirects' if limit == 3
+
+      response = Net::HTTP.start(url.host, use_ssl: true) do |http|
+        http.get url.request_uri
+      end
+
+      case response
+      when Net::HTTPRedirection
+        new_url = URI.parse(response['location'])
+        if new_url.host
+          url = new_url
+        else
+          url.path = new_url.path
+        end
+      when Net::HTTPSuccess
+        doc = Nokogiri::HTML(response.body)
+        form = doc.xpath('//form[@name="appleConnectForm"]')
+        action = form[0]['action']
+        ...
+      else
+        # response code isn't a 200; raise an exception
+        response.error!
+      end
+    end
   end
 end
